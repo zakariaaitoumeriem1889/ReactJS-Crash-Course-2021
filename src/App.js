af72587c68153829ab00f8dc395b28897f5c2109
Zakaria AITOUMERIEM <zakariaaitoumeriem1889@gmail.com>
2021-01-20 00:43:42 +0100

    Add task to server

--- src/App.js
+++ src/App.js
@@ -44,10 +44,18 @@ function App() {
     };
 
     // Add Task
-    const addTask = (task) => {
-        const id = Math.floor(Math.random() * 10000) + 1;
-        const newTask = {id, ...task};
-        setTasks([...tasks, newTask]);
+    const addTask = async (task) => {
+        const res = await fetch('http://localhost:5000/tasks', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(task),
+        })
+
+        const data = await res.json()
+
+        setTasks([...tasks, data])
     }
 
     return (
