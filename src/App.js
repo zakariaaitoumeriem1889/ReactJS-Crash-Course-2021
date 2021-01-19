c98f127167e1d3713bae23a73408efb089a0d0be
Zakaria AITOUMERIEM <zakariaaitoumeriem1889@gmail.com>
2021-01-19 21:49:05 +0100

    State & useState Hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Header from "./components/Header";
-import * as React from "react";
+import Tasks from "./components/Tasks";
 
 function App() {
     return (
         <div className="container">
-            <Header />
+            <Header/>
+            <Tasks/>
         </div>
     );
 }
