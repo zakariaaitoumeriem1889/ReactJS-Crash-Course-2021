c9145d9b910cd942b2ef20ae8e28d720c2e28d67
Zakaria AITOUMERIEM <zakariaaitoumeriem1889@gmail.com>
2021-01-20 00:09:37 +0100

    showAddTask state && Button toggle

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,24 @@
 import PropTypes from 'prop-types';
 import Button from "./Button";
 
-const Header = ({ title }) => {
-    const onClick = () => {
-        console.log('Click');
-    }
-
+const Header = ({ title, onAdd, showAddTask }) => {
     return (
         <header className="header">
             <h1>{title}</h1>
-            <Button color="green" text='Add' onClick={onClick} />
+            <Button
+                color={ showAddTask ? "red" : "green" }
+                text={ showAddTask ? "Close" : "Add" }
+                onClick={onAdd} />
         </header>
     )
 }
 
 Header.defaultProps = {
-    title: 'Task Tracker',
-}
+    title: "Task Tracker",
+};
 
 Header.propTypes = {
     title: PropTypes.string.isRequired,
-}
+};
 
 export default Header;
