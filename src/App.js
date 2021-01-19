696f9d135a7c7f26e736e99f1ddf02ffb2fdb1d7
Zakaria AITOUMERIEM <zakariaaitoumeriem1889@gmail.com>
2021-01-20 00:03:16 +0100

    Add Task Submit

--- src/App.js
+++ src/App.js
@@ -44,10 +44,17 @@ function App() {
         );
     };
 
+    // Add Task
+    const addTask = (task) => {
+        const id = Math.floor(Math.random() * 10000) + 1;
+        const newTask = {id, ...task};
+        setTasks([...tasks, newTask]);
+    }
+
     return (
         <div className="container">
             <Header/>
-            <AddTask />
+            <AddTask onAdd={addTask} />
             {tasks.length > 0 ? (
                 <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
             ) : (
