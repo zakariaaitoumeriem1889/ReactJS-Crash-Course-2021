ce001913fb3d3d04ab77c03ce9b03bd1eec47827
Zakaria AITOUMERIEM <zakariaaitoumeriem1889@gmail.com>
2021-01-20 00:46:45 +0100

    Toggle reminder on server

--- src/App.js
+++ src/App.js
@@ -12,6 +12,14 @@ function App() {
         return await res.json();
     }
 
+    // Fetch Task
+    const fetchTask = async (id) => {
+        const res = await fetch(`http://localhost:5000/tasks/${id}`)
+        const data = await res.json()
+
+        return data
+    }
+
     useEffect(() => {
         const getTasks = async () => {
             const tasksFromServer = await fetchTasks();
@@ -30,18 +38,26 @@ function App() {
     };
 
     // Toggle Reminder
-    const toggleReminder = (id) => {
+    const toggleReminder = async (id) => {
+        const taskToToggle = await fetchTask(id)
+        const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+        const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(updTask),
+        })
+
+        const data = await res.json()
+
         setTasks(
             tasks.map((task) =>
-                task.id === id
-                    ? {
-                        ...task,
-                        reminder: !task.reminder,
-                    }
-                    : task
+                task.id === id ? { ...task, reminder: data.reminder } : task
             )
-        );
-    };
+        )
+    }
 
     // Add Task
     const addTask = async (task) => {
