97072074d65392c0ce5129073e117cae0a209b14
Zakaria AITOUMERIEM <zakariaaitoumeriem1889@gmail.com>
2021-01-19 21:08:23 +0100

    Expressions in JSX

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 function App() {
+    // const name = 'Brad';
+    // const x = true;
+
     return (
         <div className="container">
             <h1>Hello From React</h1>
-            <h2>Hello</h2>
+            {/*<h2>Hello { name }</h2>*/}
+            {/*<h2>Hello { x ? 'Yes' : 'No' }</h2>*/}
         </div>
     );
 }
