4269041a5ad54100d20cef30a53c49caeba1ed1e
Zakaria AITOUMERIEM <zakariaaitoumeriem1889@gmail.com>
2021-01-19 23:36:46 +0100

    Delete task & prop drilling

--- src/App.js
+++ src/App.js
@@ -24,10 +24,15 @@ function App() {
         },
     ]);
 
+    // Delete Task
+    const deleteTask = (id) => {
+        setTasks(tasks.filter((task) => task.id !== id))
+    }
+
     return (
         <div className="container">
             <Header/>
-            <Tasks tasks={tasks} />
+            <Tasks tasks={tasks} onDelete={deleteTask} />
         </div>
     );
 }
